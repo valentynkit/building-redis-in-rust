2f8285d86e1e74dc3b2447265d5e7c375459afb7
Valentyn Kit <valentynkit.work@pm.me>
2026-06-23 19:43:17 +0300

    feat(command): enum refactor to use strum carog

--- src/command.rs
+++ src/command.rs
@@ -1,11 +1,11 @@
 use crate::db::{Db, Key, Value};
+use crate::resp::{self, ResponseKind};
 use std::{
     env::args,
     io::{self},
 };
+use strum::{AsRefStr, EnumString};
 use thiserror::Error;
-
-use crate::resp::{self, ResponseKind};
 
 #[derive(Debug, Error)]
 pub enum CommandError {
@@ -15,44 +15,49 @@ pub enum CommandError {
     WrongArity(String, String),
 }
 
+#[derive(AsRefStr, EnumString)]
+#[strum(serialize_all = "UPPERCASE", ascii_case_insensitive)]
 pub enum Command {
     Ping,
     Echo,
     Set,
     Get,
 }
-impl Command {
-    pub fn to_string(&self) -> String {
-        match self {
-            Self::Ping => "PING".to_owned(),
-            Self::Echo => "ECHO".to_owned(),
-            Self::Set => "SET".to_owned(),
-            Self::Get => "GET".to_owned(),
-        }
-    }
-    // TODO: make caseinsensitive
-    pub fn from_string(name: &[u8]) -> Option<Self> {
-        match name {
-            b"PING" => Some(Self::Ping),
-            b"ECHO" => Some(Self::Echo),
-            b"SET" => Some(Self::Set),
-            b"GET" => Some(Self::Get),
-            _ => None,
-        }
-    }
 
+#[derive(AsRefStr, EnumString)]
+#[strum(serialize_all = "UPPERCASE", ascii_case_insensitive)]
+pub enum ExpCmd {
+    Ex,
+    Px,
+}
+
+impl ExpCmd {
+    fn from_bytes(value: &[u8]) -> Option<Self> {
+        str::from_utf8(value).ok()?.parse().ok()
+    }
+}
+
+impl Command {
     fn check_arity(&self, argc: usize) -> Result<(), CommandError> {
         let arity = self.arity();
         if (arity > 0 && argc != arity as usize) || (arity < 0 && argc < (-arity) as usize) {
-            return Err(CommandError::WrongArity(self.to_string(), argc.to_string()));
+            return Err(CommandError::WrongArity(
+                self.as_ref().to_owned(),
+                argc.to_string(),
+            ));
         }
         Ok(())
     }
+
+    fn from_bytes(value: &[u8]) -> Option<Self> {
+        str::from_utf8(value).ok()?.parse().ok()
+    }
+
     fn arity(&self) -> i32 {
         match self {
             Command::Ping => 1,
             Command::Echo => -1,
-            Command::Set => 3,
+            Command::Set => -3,
             Command::Get => 2,
         }
     }
@@ -63,7 +68,7 @@ pub fn dispatch(db: &mut Db, args: &[Vec<u8>], out: &mut Vec<u8>) -> Result<(), 
     let name = args
         .first()
         .ok_or_else(|| CommandError::Unknown(String::new()))?;
-    let cmd = Command::from_string(&args[0])
+    let cmd = Command::from_bytes(&args[0])
         .ok_or_else(|| CommandError::Unknown(String::from_utf8_lossy(name).into_owned()))?;
     cmd.check_arity(args.len())?;
 
@@ -72,7 +77,11 @@ pub fn dispatch(db: &mut Db, args: &[Vec<u8>], out: &mut Vec<u8>) -> Result<(), 
         // TODO after resp update
         Command::Echo => resp::write_out(ResponseKind::BULK(&args[1..].join(&b' ')), out),
         Command::Set => {
-            cmd_set(db, &args[1], &args[2]);
+            if (args.len() > 3) {
+                cmd_setex(db, &args[1], &args[2], &args[3], &args[4]);
+            } else {
+                cmd_set(db, &args[1], &args[2]);
+            }
             resp::write_out(ResponseKind::SIMPLE_OK, out);
         }
         Command::Get => match cmd_get(db, &args[1]) {
@@ -87,6 +96,12 @@ fn cmd_get(db: &Db, key: &Vec<u8>) -> Option<Vec<u8>> {
     let key: Key = key.into();
     let value = db.get(&key)?; // None → key absent → caller writes $-1
     Some(value.into()) // &Value → Vec<u8> via the reverse From impl
+}
+
+fn cmd_setex(db: &mut Db, key: &Vec<u8>, value: &Vec<u8>, exp_cmd: &Vec<u8>, exp: &Vec<u8>) {
+    cmd_set(db, key, value);
+
+    // exp_cmd could be EX or PX. EX = seconds, PX = milisseconds.
 }
 fn cmd_set(db: &mut Db, key: &Vec<u8>, value: &Vec<u8>) {
     let key: Key = key.into();
