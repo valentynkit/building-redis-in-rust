d3dba69fd574d8af8f69ec04526c66db80979b38
Valentyn Kit <valentynkit.work@pm.me>
2026-07-07 16:11:57 +0300

    codecrafters submit [skip ci]

--- src/command/common.rs
+++ src/command/common.rs
@@ -13,7 +13,7 @@ pub enum CommandError {
     WrongNumber(String),
     #[error("wrong argument format, could not parse stream: expected stream. actual '{0}'")]
     ParseStream(String),
-    #[error("The ID specified in XADD is equal or smaller than the target stream top item.")]
+    #[error("The ID specified in XADD is equal or smaller than the target stream top item")]
     InvalidStream,
     #[error("key already exist with different type, expected: '{0}'")]
     WrongType(String),
